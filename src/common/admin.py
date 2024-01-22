211490bfc375e4e8fad22dd2a8c200755b01748a
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 16:01:36 +0100

    Create FontAwesomeIconAdmin

--- src/common/admin.py
+++ src/common/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import FontAwesomeIcon
 
-# Register your models here.
+
+@admin.register(FontAwesomeIcon)
+class FontAwesomeIconAdmin(admin.ModelAdmin):
+    list_display = ('title', 'classes', 'rendered_html')
+    list_editable = ('classes', )
+
+    def rendered_html(self, obj):
+        return obj.html()
+
+    rendered_html.short_description = 'Rendered HTML'
+
