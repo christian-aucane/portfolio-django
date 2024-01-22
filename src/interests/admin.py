5d4366bb0a93c648080a899ccc63d86f0a5aaa87
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 17:01:58 +0100

    Create ParagraphAdmin

--- src/interests/admin.py
+++ src/interests/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Paragraph
 
-# Register your models here.
+
+@admin.register(Paragraph)
+class ParagraphAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'display_order')
+    list_editable = ('display_order',)
