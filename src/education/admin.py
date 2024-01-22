4460f610b72ba8fced4f2fcba2a9916453fa5817
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 15:21:08 +0100

    Create EducationAdmin

--- src/education/admin.py
+++ src/education/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Education
 
-# Register your models here.
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('school_name', 'program', 'start_date', 'end_date')
+    search_fields = ('school_name', 'program', 'description')
