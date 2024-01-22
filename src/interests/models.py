3608181841978023ef7bcd3cce779f979ccc18c9
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 17:07:39 +0100

    Add UNIQUE constraint in title

--- src/interests/models.py
+++ src/interests/models.py
@@ -9,8 +9,11 @@ class Paragraph(DisplayOrderBaseModel):
         verbose_name = _("Paragraph")
         verbose_name_plural = _("Paragraphs")
 
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     text = models.TextField()
 
     def __str__(self):
         return f"{self.title} - {_('Paragraph')} {self.display_order}"
+
+    def html(self):
+        return f"<p>{self.text}</p>" if self.text else ""
