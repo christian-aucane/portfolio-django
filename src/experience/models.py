4faafceb03fd59fab4b3c67998862a4cd1574aab
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 14:50:47 +0100

    Create Experience model

--- src/experience/models.py
+++ src/experience/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Experience(models.Model):
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    company = models.CharField(max_length=255, verbose_name=_("Company"))
+    description = models.TextField(verbose_name=_("Description"))
+    start_date = models.DateField(verbose_name=_("Start Date"))
+    end_date = models.DateField(verbose_name=_("End Date"), null=True, blank=True)
+    thumbnail = models.ImageField(upload_to='experience_images/', verbose_name=_("Image"))
+
+    def __str__(self):
+        return f"{self.title} at {self.company}"
+
+    def get_thumbnail_url(self):
+        return self.thumbnail.url
