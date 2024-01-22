86d0ec34d1c5357dcfb47eb8f2de904b5d7923aa
Christian Aucane <127300350+Stargate136@users.noreply.github.com>
2024-01-22 17:09:11 +0100

    Interests (#7)
    * Create 'about' app
    
    * Create models
    
    * Create admin classes
    
    * Update TODOs
    
    * Write tests
    
    * Create migration to automaticly create a AboutInfo when migrate
    
    * Update README
    
    * Create 'interests' app
    
    * Create Paragraph model
    
    * Create ParagraphAdmin
    
    * Add UNIQUE constraint in title
    
    * Write tests

---
+++ src/interests/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from base.models import DisplayOrderBaseModel
+
+
+class Paragraph(DisplayOrderBaseModel):
+    class Meta:
+        verbose_name = _("Paragraph")
+        verbose_name_plural = _("Paragraphs")
+
+    title = models.CharField(max_length=255, unique=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} - {_('Paragraph')} {self.display_order}"
+
+    def html(self):
+        return f"<p>{self.text}</p>" if self.text else ""
