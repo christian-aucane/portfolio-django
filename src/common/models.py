6837dbfb6c79bf847b770e5303a672d4c6a85f00
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 16:01:16 +0100

    Create FontAwesomIcon model

--- src/common/models.py
+++ src/common/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class FontAwesomeIcon(models.Model):
+    class Meta:
+        verbose_name = _("Font Awesome Icon")
+        verbose_name_plural = _("Font Awesome Icons")
+
+    title = models.CharField(max_length=255, unique=True, verbose_name=_("Name"))
+    classes = models.CharField(max_length=255, verbose_name=_("Font Awesome Icon Classes"))
+
+    def __str__(self):
+        return self.title
+
+    def html(self):
+        return f'<i class="{self.classes}" title={self.title}></i>'
+
+    def li_html(self, content=""):
+        return f'<li><i class="fa-li {self.classes}" title={self.title}></i>{content}</li>'
