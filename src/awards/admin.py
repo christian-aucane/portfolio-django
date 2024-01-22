966be148aa6368a119e2f6a0970e4abc2c6df0ba
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 18:04:24 +0100

    Create CategoryAwardAdmin and AwardAdmin

--- src/awards/admin.py
+++ src/awards/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AwardCategory, Award
+
+
+@admin.register(AwardCategory)
+class AwardCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_order')
+    list_editable = ('display_order',)
+
+
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'display_order')
+    search_fields = ('title', 'category__title')
+    list_filter = ('category__title',)
+    list_editable = ('display_order',)
