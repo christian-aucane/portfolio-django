417c66199131e10a9b620b394f027e9967d2ea67
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 17:01:46 +0100

    Create Paragraph model

--- src/interests/models.py
+++ src/interests/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from base.models import DisplayOrderBaseModel
+
+
+class Paragraph(DisplayOrderBaseModel):
+    class Meta:
+        verbose_name = _("Paragraph")
+        verbose_name_plural = _("Paragraphs")
+
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} - {_('Paragraph')} {self.display_order}"
