2bb69b130248243d8d7fc5c14c3750194b2bb548
Christian Aucane <christianaucane1@gmail.com>
2024-01-22 14:07:40 +0100

    Create admin classes

--- src/about/admin.py
+++ src/about/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AboutInfo, AboutSkill, SocialLink
 
-# Register your models here.
+
+class AboutSkillInline(admin.TabularInline):
+    model = AboutSkill
+    extra = 0
+
+
+class SocialLinkInline(admin.TabularInline):
+    model = SocialLink
+    extra = 0
+
+
+@admin.register(AboutInfo)
+class AboutInfoAdmin(admin.ModelAdmin):
+    inlines = [AboutSkillInline, SocialLinkInline]
