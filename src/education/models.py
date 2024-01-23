c4b3cc50a76309fc1cb280656c1ebbc76af9ba0c
Christian Aucane <christianaucane1@gmail.com>
2024-01-23 09:39:24 +0100

    Change thumbnails folders name

--- src/education/models.py
+++ src/education/models.py
@@ -13,7 +13,7 @@ class Education(DurationBaseModel):
     program = models.CharField(max_length=255, verbose_name=_("Program"))
     role = models.CharField(max_length=255, verbose_name=_("Role"))
     description = models.TextField(verbose_name=_("Description"))
-    thumbnail = models.ImageField(upload_to='education_images/', verbose_name=_("Thumbnail"))
+    thumbnail = models.ImageField(upload_to='education_thumbnails/', verbose_name=_("Thumbnail"))
 
     def __str__(self):
         return f"{self.school_name} - {self.program}"
