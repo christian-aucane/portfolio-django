27492bd8256a6689a7f9be615e1002dfdd2383e5
Christian Aucane <christianaucane1@gmail.com>
2024-01-24 11:48:32 +0100

    Create ContactMessageInline and ContactThreadAdmin

--- src/contact/admin.py
+++ src/contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import ContactMessage, ContactThread
+
+
+class ContactMessageInline(admin.TabularInline):
+    model = ContactMessage
+    extra = 0
+    readonly_fields = ('sender', 'created_at', 'message')
+
+
+@admin.register(ContactThread)
+class ContactThreadAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at', 'processed_at', 'archived_at', 'is_processed', 'is_archived')
+    list_editable = ('is_processed', 'is_archived')
+    inlines = [ContactMessageInline]
+    readonly_fields = ('name', 'email', 'subject', 'created_at', 'processed_at', 'archived_at')
